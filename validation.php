fc7803180bf235f3180330e04449c775fceb7bf0
CDamasse <charles.damasse@gmail.com>
2018-04-18 11:25:15 +0200

    Ajout des balises

--- validation.php
+++ validation.php
@@ -6,7 +6,6 @@ session_start();
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
 
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
